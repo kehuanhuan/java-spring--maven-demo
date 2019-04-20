647aab379e82901998ac12a8fe40cdb0d286299d
kehuanhuan <1152018701@qq.com>
2019-04-20 17:13:14 +0800

    FIX

--- spring-demo/src/main/java/khhcom/hello/Application.java
+++ spring-demo/src/main/java/khhcom/hello/Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import com.khh.tools.App;
 
 @SpringBootApplication
 public class Application {
@@ -16,18 +15,4 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            App.whoami();
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
 }
