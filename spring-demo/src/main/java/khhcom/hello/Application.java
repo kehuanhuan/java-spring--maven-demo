e4dbe951b3534a5cf7c8f72f211ea494fa19a458
kehuanhuan <1152018701@qq.com>
2019-06-24 21:03:02 +0800

    new

--- spring-demo/src/main/java/khhcom/hello/Application.java
+++ spring-demo/src/main/java/khhcom/hello/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.mybatis.spring.annotation.MapperScan;
 
+import org.mybatis.spring.annotation.MapperScan;
 @MapperScan("khhcom.mapper")
 @SpringBootApplication
 public class Application {
--- spring-demo/src/main/java/khhcom/hello/controller/HelloController.java
+++ spring-demo/src/main/java/khhcom/hello/controller/HelloController.java
@@ -1,10 +1,21 @@
 package khhcom.hello.controller;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import khhcom.mapper.UserMapper;
+import khhcom.model.User;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
+
+    @Autowired
+    private UserMapper userMapper;
 
     @RequestMapping(value="/", method = RequestMethod.GET)
     public String index() {
@@ -12,7 +23,14 @@ public class HelloController {
     }
 
     @RequestMapping(value="/hello", method = RequestMethod.GET)
-    public String show() {
-        return "hello";
+    public Object show() {
+
+        QueryWrapper queryWrapper = new QueryWrapper<User>();
+        queryWrapper.ge("age", 23);
+
+        Page<User> page = new Page<User>(1, 2);
+        IPage<User> iPage = userMapper.selectPage(page, queryWrapper);
+
+        return iPage;
     }
 }
