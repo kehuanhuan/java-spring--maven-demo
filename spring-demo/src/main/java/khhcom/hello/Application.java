618ee0d61d643e7c2aa785f52942ccc4533b2d7f
kehuanhuan <1152018701@qq.com>
2019-06-27 07:46:57 +0800

    new

--- spring-demo/src/main/java/khhcom/hello/Application.java
+++ spring-demo/src/main/java/khhcom/hello/Application.java
@@ -1,14 +1,22 @@
 package khhcom.hello;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 
 import org.mybatis.spring.annotation.MapperScan;
+
 @MapperScan("khhcom.mapper")
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
